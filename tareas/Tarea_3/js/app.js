8ee5c1a83dbb877c4f0ccd8eb82c8dfa565714a7
pachavarriac <pachavarriac@ucenfotec.ac.cr>
2025-07-04 21:58:43 -0600

    Tarea 3: Finalizada - 04/07/2025 21:58

--- tareas/Tarea_3/js/app.js
+++ tareas/Tarea_3/js/app.js
@@ -8,11 +8,19 @@ document.querySelectorAll('#mainMenu .collapse').forEach(function (submenu) {
     });
 });
 
-// document.querySelectorAll('.custom-accordion-button').forEach(button => {
-//     button.addEventListener('click', () => {
-//         const iconPlus = button.querySelector('.icon-plus');
-//         const iconMinus = button.querySelector('.icon-minus');
-//         iconPlus.classList.toggle('d-none');
-//         iconMinus.classList.toggle('d-none');
-//     });
-// });
+const btn = document.getElementById("btn-top");
+
+// Mostrar el botón si se hace scroll hacia abajo
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 500) {
+        btn.classList.remove("d-none");
+    } else {
+        btn.classList.add("d-none");
+    }
+});
+
+// Al hacer clic, volver arriba con scroll suave
+btn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
